6162891b3363b923e66013e0ce9aab212ba9b4d8
abhmul <abhmul@users.noreply.github.com>
2022-11-01 22:21:32 +0000

    deploy: 519e264f410fd83f5a48a7f6e9df772300bc3a5d

---
+++ scripts/process.sh
@@ -0,0 +1,3 @@
+python scripts/obsidian_manipulator.py -mt "GraphExclude"
+python scripts/obsidian_manipulator.py -at "gitignore" --regex_filter "(.*\d\d\d\d-\d\d-\d\d.*\.md|Journal\.md|ISIT Fixes and Camera Ready Prep\.md|Masters Thesis Outline\.md|Masters Thesis\.md|Math 539 - Functional Analysis\.md|MATH 549 - Differentiable Manifolds\.md|MCS 521 - Combinatorial Optimization\.md|MCS 521 Final Exam\.md|MCS 521 Research Paper Project\.md|Research Notes\.md|Research Paper Project - Notes on Introduction\.md|Stat 502 - Probability II\.md)"
+python scripts/obsidian_manipulator.py -cg
